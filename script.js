a6c03a419c779f305767bd6f2663e028b8b8d04c
arijiiiitttt🍉 <149235252+arijiiiitttt@users.noreply.github.com>
2024-07-14 10:00:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,5 +3,5 @@ window.onload = function() {
   setTimeout(function() {
     document.getElementById("loader").style.display = "none";
     document.getElementById('content').style.display = "block";
-  }, 5000);
+  }, 6000);
 };
